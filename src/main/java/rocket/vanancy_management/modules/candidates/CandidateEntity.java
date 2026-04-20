76ba9e8deaa5461173772ff67e72c729cc609cc4
Marcus Vynicius <viniciusfla1999@gmail.com>
2026-04-20 22:39:25 +0100

    feat: practicing validations, exceptions and DataBase

--- src/main/java/rocket/vanancy_management/modules/candidates/CandidateEntity.java
+++ src/main/java/rocket/vanancy_management/modules/candidates/CandidateEntity.java
@@ -1,6 +1,10 @@
 package rocket.vanancy_management.modules.candidates;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -9,8 +13,15 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "Usernames should not contain spaces.")
     private String username;
+
+    @Email(message = "Please enter a valid email address.")
     private String email;
+
+    @Length(min = 6, max = 20)
     private String password;
     private String description;
     private String curriculum;
--- src/main/java/rocket/vanancy_management/modules/candidates/controllers/CandidateController.java
+++ src/main/java/rocket/vanancy_management/modules/candidates/controllers/CandidateController.java
@@ -1,5 +1,6 @@
 package rocket.vanancy_management.modules.candidates.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 import rocket.vanancy_management.modules.candidates.CandidateEntity;
 
@@ -8,7 +9,7 @@ import rocket.vanancy_management.modules.candidates.CandidateEntity;
 public class CandidateController {
 
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidate");
         System.out.println(candidateEntity.getEmail());
     }
