de1aaaa643d5ae4203d0f2f62ff7d97ffc7ca6e8
Marcus Vynicius <viniciusfla1999@gmail.com>
2026-04-28 21:49:14 +0100

    getting and validation token

--- src/main/java/rocket/vanancy_management/modules/company/services/AuthCompanyService.java
+++ src/main/java/rocket/vanancy_management/modules/company/services/AuthCompanyService.java
@@ -12,6 +12,9 @@ import org.springframework.stereotype.Service;
 import rocket.vanancy_management.modules.company.dto.AuthCompanyDTO;
 import rocket.vanancy_management.modules.company.repositories.CompanyRepository;
 
+import java.time.Duration;
+import java.time.Instant;
+
 @Service
 public class AuthCompanyService {
 
@@ -41,6 +44,7 @@ public class AuthCompanyService {
         //If passwords matches -> Token generate
         Algorithm algorithm = Algorithm.HMAC256(secretKey);
         var token = JWT.create().withIssuer("javagas")
+                .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
                 .withSubject(company.getId().toString())
                 .sign(algorithm);
 
