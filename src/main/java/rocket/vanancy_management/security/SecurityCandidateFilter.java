95bfea8430534dafd76d66a4a45a370d70ec63b1
Marcus Vynicius <viniciusfla1999@gmail.com>
2026-05-01 22:10:19 +0100

    refactoring code

--- src/main/java/rocket/vanancy_management/security/SecurityCandidateFilter.java
+++ src/main/java/rocket/vanancy_management/security/SecurityCandidateFilter.java
@@ -5,12 +5,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import rocket.vanancy_management.providers.JWTCandidateProvider;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 public class SecurityCandidateFilter extends OncePerRequestFilter {
@@ -24,7 +27,7 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
 
-        SecurityContextHolder.getContext().setAuthentication(null);
+        //SecurityContextHolder.getContext().setAuthentication(null);
         String header = request.getHeader("Authorization");
 
         if(request.getRequestURI().startsWith("/candidate")) {
@@ -40,7 +43,15 @@ public class SecurityCandidateFilter extends OncePerRequestFilter {
                 }
 
                 request.setAttribute("candidate_id", token.getSubject());
-                var roles = token.getClaim("roles");
+                var roles = token.getClaim("roles").asList(Object.class);
+
+                var grants = roles.stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.toString().toUpperCase()))
+                        .toList();
+
+                UsernamePasswordAuthenticationToken auth =
+                        new UsernamePasswordAuthenticationToken(token.getSubject(), null, grants);
+                SecurityContextHolder.getContext().setAuthentication(auth);
             }
         }
 
